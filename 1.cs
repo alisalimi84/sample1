9b369acce9b474d17d142eeede70f00505a7d5f8
alisalimi84 <108054595+alisalimi84@users.noreply.github.com>
2022-07-13 22:42:43 +0430

    Add files via upload

---
+++ 1.cs
@@ -0,0 +1,9 @@
+﻿int Number=0;
+System.Console.WriteLine("Please Enter your Number");
+int num=int.Parse(Console.ReadLine());
+
+for (int i = 0; i <=num; i++)
+{
+    Number=Number+i;
+    
+}System.Console.WriteLine(Number);
